1373bf27a5aa98a0ded1bac6b3974e1bb3d612ee
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-06-19 08:48:24 +0100

    :put_litter_in_its_place: Remove unused functions

--- app/lib/utils/utils.js
+++ app/lib/utils/utils.js
@@ -1,37 +1,11 @@
-const constants = require('../constants');
-
 function joinTruthyValues(obj) {
   return Object.values(obj)
     .filter(value => value)
     .join(', ');
 }
 
-function getValues(myHash) {
-  const keys = Object.keys(myHash);
-
-  return keys.map(v => myHash[v]);
-}
-
-function getAlternativeTypeFor(type) {
-  if (type === constants.serviceTypes.professional) {
-    return constants.serviceTypes.kit;
-  } else if (type === constants.serviceTypes.kit) {
-    return constants.serviceTypes.professional;
-  }
-  return type;
-}
-
 function areEqual(queryItem, item) {
   return (queryItem.localeCompare(item, 'en', { sensitivity: 'base' }) === 0);
-}
-
-function isProfessionalChoice(query) {
-  return (areEqual(query.type, constants.serviceTypes.professional)
-    && (getValues(constants.serviceChoices).includes(query.origin)));
-}
-
-function getResultsInternalUrl(query, location) {
-  return `${constants.siteRoot}/results?location=${location}&type=${getAlternativeTypeFor(query.type)}&origin=${query.origin}`;
 }
 
 function trim(string) {
@@ -40,8 +14,6 @@ function trim(string) {
 
 module.exports = {
   areEqual,
-  getResultsInternalUrl,
-  isProfessionalChoice,
   joinTruthyValues,
   trim,
 };
