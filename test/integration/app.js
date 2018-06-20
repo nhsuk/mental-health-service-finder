fa024339927538e2d8aa5d5d5213c5decee8f1e4
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-06-20 09:48:30 +0100

    :customs: Move security header test

--- test/integration/app.js
+++ test/integration/app.js
@@ -16,6 +16,18 @@ describe('Application behaviour', () => {
     expect(res).to.redirect;
     expect(res.req.path).to.equal(`${constants.siteRoot}/`);
   });
+
+  it('should have or have not headers for security', async () => {
+    const res = await chai.request(server)
+      .get(`${constants.siteRoot}`);
+    expect(res).to.have.header('Content-Security-Policy', 'child-src *.hotjar.com; connect-src \'self\' *.hotjar.com:*; default-src \'self\'; font-src *.nhs.uk *.hotjar.com; img-src \'self\' data: *.google-analytics.com *.hotjar.com *.webtrends.com *.webtrendslive.com *.nhs.uk; script-src \'self\' \'unsafe-eval\' \'unsafe-inline\' data: *.google-analytics.com *.hotjar.com *.webtrends.com *.webtrendslive.com; style-src \'self\' \'unsafe-inline\' *.nhs.uk');
+    expect(res).to.have.header('X-Xss-Protection', '1; mode=block');
+    expect(res).to.have.header('X-Frame-Options', 'DENY');
+    expect(res).to.have.header('X-Content-Type-Options', 'nosniff');
+    expect(res).to.not.have.header('X-Powered-By');
+    expect(res).to.have.header('X-Download-Options', 'noopen');
+    expect(res).to.have.header('Strict-Transport-Security', 'max-age=15552000');
+  });
 });
 
 describe('An unknown page', () => {
