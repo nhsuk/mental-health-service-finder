f06bd8f2130d3396bd4625883fcf1243ad1bcbe8
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-06-20 17:27:14 +0100

    :volcano: Initial integration with API

--- app/middleware/getGPs.js
+++ app/middleware/getGPs.js
@@ -9,43 +9,51 @@ function getGPs(req, res, next) {
   // TODO: Request GPs
   // Process results
   // Render results
-  res.locals.services = [
-    { ccg: '00A', name: 'GP result one' },
-    { ccg: '99Q', name: 'GP result two' },
-  ];
+  // res.locals.services = [
+  //   { ccg: '00A', name: 'GP result one' },
+  //   { ccg: '99Q', name: 'GP result two' },
+  // ];
 
+  const query = req.query.query;
   const apiKey = config.api.key;
   const apiVersion = config.api.version;
   const apiHost = config.api.host;
   const apiOrgIndex = config.api.indexes.orgLookup;
 
   const options = {
+    // TODO: Build this somewhere else
     body: JSON.stringify({
-      count: true,
-      search: '*',
+      filter: 'OrganisationTypeID eq \'GPB\'',
+      search: query, // TODO: Handle no input. Maybe change the name
+      searchFields: 'OrganisationName,OrganisationAliases,Postcode',
+      select: 'OrganisationName,Address1,Address2,Address3,City,County,Postcode,CCG',
+      suggesterName: 'orgname-suggester',
       top: 10,
     }),
     headers: {
       'Content-Type': 'application/json',
       'api-key': apiKey,
     },
-    url: `https://${apiHost}/indexes/${apiOrgIndex}/docs/search?api-version=${apiVersion}`,
+    url: `https://${apiHost}/indexes/${apiOrgIndex}/docs/suggest?api-version=${apiVersion}`,
   };
   log.debug(options);
 
   request.post(options, (error, response, body) => {
-    log.error(error);
-      const pbody = JSON.parse(body);
-      log.debug(pbody);
+    // TODO: Test around an empty body - what happens when 403?
+    const pbody = JSON.parse(body);
+    log.debug(pbody);
+    console.log(pbody);
     if (!error && response.statusCode === 200) {
-      // const pbody = JSON.parse(body);
-      log.debug(pbody);
+      const results = pbody.value;
+      // TODO: The address needs stitching together
+      res.locals.results = results || [];
+      next();
     } else {
-      log.error(response.statusCode);
       log.error('ERROR!');
+      log.error(error);
+      next('error');
     }
   });
-  next();
   // const location = res.locals.location;
 
   // log.debug({ location }, 'Postcode search text');
