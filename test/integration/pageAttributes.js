d8c69ccd6f6e0d32b0236e2c3b6e8176a0876df4
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-06-28 15:15:23 +0100

    :ballot_box_with_check: Ensure page attribute tests are run

--- test/integration/pageAttributes.js
+++ test/integration/pageAttributes.js
@@ -3,8 +3,9 @@ const chaiHttp = require('chai-http');
 const cheerio = require('cheerio');
 
 const constants = require('../../app/lib/constants');
+const deepClone = require('../../app/lib/utils/utils').deepClone;
 const iExpect = require('../lib/expectations');
-const routes = require('../../config/routes');
+const routes = deepClone(require('../../config/routes'));
 const server = require('../../server');
 
 const expect = chai.expect;
@@ -12,8 +13,9 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Page attributes', () => {
-  const testRoutes = Object.keys(delete routes[routes.results]);
-  testRoutes.forEach(async (route) => {
+  delete routes.results;
+
+  Object.keys(routes).forEach(async (route) => {
     const path = routes[route].path;
 
     const res = await chai.request(server).get(`${constants.siteRoot}${path}`);
@@ -42,7 +44,7 @@ describe('Page attributes', () => {
       });
 
       describe('meta tags', () => {
-        if (path !== routes.start) {
+        if (path !== routes.start.path) {
           it('should include a robots noindex directive for all non \'start\' pages', () => {
             expect($('meta[name=robots]').prop('content')).to.equal('noindex');
           });
