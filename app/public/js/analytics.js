bfc63d7b6d2a84d35573d605d11cfa03601e02db
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-07-11 15:35:12 +0100

    :bug: Fix unreferenced Webtrends

--- app/public/js/analytics.js
+++ app/public/js/analytics.js
@@ -1,9 +1,8 @@
 ((global) => {
   const $ = global.jQuery;
-  const WT = global.Webtrends;
 
   function trackClick(element, arg1, arg2) {
-    WT.multiTrack({ argsa: [arg1, arg2, 'WT.dl', '121'], element });
+    Webtrends.multiTrack({ argsa: [arg1, arg2, 'WT.dl', '121'], element });
   }
 
   const trackingName = 'DCSext.IAPT';
