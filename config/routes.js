53268f7b85d2755d7e5a1a93fb6b51103e1fb6b9
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2018-08-14 10:20:19 +0100

    :question: Test search heading

--- config/routes.js
+++ config/routes.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   search: {
     'WT.si_p': 'GP Search',
-    h1: 'We need to know your GP surgery to get you to the right service',
+    h1: 'Tell us your GP',
     path: '/search',
     title: 'Search for your GP',
   },
