c82bf6483f322afd273e4a2cc4650faa40372929
Tom Doughty <thomashdoughty@gmail.com>
2019-10-15 10:49:17 +0100

    :zap: Remove redundant JS files

--- app/public/js/init.js
+++
@@ -1,7 +0,0 @@
-// Require all modules neeeded for the application
-require('./detailsPolyfill');
-require('./headerMobileMenuToggle');
-require('./headerMobileSearchToggle');
-require('./headerSearchTypeahead');
-require('./querySearchTypeahead');
-require('./skiplinkFix');
